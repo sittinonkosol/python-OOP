9cd8f7b131aa1743e1d2ac8a5348eb0f8aaa6e68
shitthinon <kosol0636618792@gmail.com>
2025-12-26 09:28:05 +0700

    Fix player dimensions and initialization in Game class; correct display mode setup and enhance player movement logic.

--- pygame/assets/bg-move.py
+++ pygame/assets/bg-move.py
@@ -6,17 +6,19 @@ class Game:
     SCREEN_H = 600
     SCREEN_SIZE = (SCREEN_W,SCREEN_H)
 
-    PLAYER_W = 24
-    PLAYER_H = 47
+    PLAYER_W = 24 * 2
+    PLAYER_H = 47 * 2
     PLAYER_SIZE = (PLAYER_W,PLAYER_H)
-    PLAYER_X = 0
-    PLAYER_Y = 0
+    PLAYER_X = 387
+    PLAYER_Y = 414
+    move = 3
 
     clock = pygame.time.Clock()
+    FPS = 60
 
     def __init__(self):
         pygame.display.set_caption("OH I CAN MOVE")
-        self.screen = pygame.display.set_move((self.SCREEN_W,self.SCREEN_H))
+        self.screen = pygame.display.set_mode((self.SCREEN_W,self.SCREEN_H))
 
         player_img = pygame.image.load(os.path.join('pygame', 'assets', 'img', 'player.png'))
         self.player = pygame.transform.scale(player_img, self.PLAYER_SIZE)
@@ -30,11 +32,29 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            self.screen.blit(self.background, (0,0))
+
+            self.player_draw()
+            self.player_move()
             
-            self.player()
+            pygame.display.update()
+            self.clock.tick(self.FPS)
     
+    def player_draw(self):
+        self.screen.blit(self.player, (self.PLAYER_X,self.PLAYER_Y))
     def player_move(self):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_LEFT] and self.PLAYER_X >= 0:
+            self.PLAYER_X -= self.move
+        if keys[pygame.K_RIGHT] and self.PLAYER_X <= self.PLAYER_X + self.PLAYER_W:
+            self.PLAYER_X += self.move
         if keys[pygame.K_UP] and self.PLAYER_Y >= 0:
-            
+            self.PLAYER_Y -= self.move
+        if keys[pygame.K_DOWN] and self.PLAYER_Y <= self.PLAYER_Y + self.PLAYER_H:
+            self.PLAYER_Y += self.move
+
+if __name__ == "__main__":
+    app = Game()
+    app.run()
